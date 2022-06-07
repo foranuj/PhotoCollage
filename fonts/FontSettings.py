75168e6a07586357f03947077fd1f99b6e30d833
Anuj <foranuj@yahoo.com>
2022-06-06 17:20:43 -0700

    Setting up MC cover.

--- fonts/FontSettings.py
+++ fonts/FontSettings.py
@@ -9,3 +9,4 @@ TEXT_FONT_SMALL = ImageFont.truetype(os.path.join(FONT_DIR, "open-sans/OpenSans-
 TITLE_FONT_MOHAVE = ImageFont.truetype(os.path.join(FONT_DIR, "Mohave/Mohave-SemiBold.ttf"), 100)
 TITLE_FONT_SELIMA = ImageFont.truetype(os.path.join(FONT_DIR, "selima/selima_.otf"), 135)
 TITLE_FONT_ECZAR = ImageFont.truetype(os.path.join(FONT_DIR, "Eczar", "static", "Eczar-SemiBold.ttf"), 105)
+TITLE_FONT_SIGNIKA = ImageFont.truetype(os.path.join(FONT_DIR, "Signika", "Signika-Bold.ttf"), 105)
